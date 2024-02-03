fd83bc6b8fe57964ecefe2867223456cb02fa221
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-03 18:41:06 +0600

    helloworld

--- 01-HelloWorld/main.js
+++ 01-HelloWorld/main.js
@@ -1,11 +1,20 @@
+// 1. import express
+
 const express = require("express");
 
+// 2. we will now create an application
+
 const app = express();
-const port = 6000;
+
+// 3. listen the app to a port
+
+const port = 8000;
 app.listen(port, () => {
-   console.log(`server success`);
+   console.log(`Server running successfully at port: ${port}`);
 });
 
+// 4. get request to root directory and send response
+
 app.get("/", (req, res) => {
-   res.send("Hello World! Express JS! Noor Tushar Khan");
+   res.send("Hello World! First Express JS response.");
 });
